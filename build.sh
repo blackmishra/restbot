25c6265ac6309e3de74069be2db581a97c05e9b9
blackmishra <mishra.shashank9598@gmail.com>
2023-03-30 19:53:15 +0530

    Updated build file

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+
+pip install python==3.8
+
 echo "Installing the latest version of poetry..."
 
 pip install --upgrade pip
@@ -12,6 +15,7 @@ rm poetry.lock
 poetry lock
 
 python -m poetry install
+
 
 python manage.py collectstatic --no-input
 python manage.py migrate
