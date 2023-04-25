baf987476f1ae0bb29cb97043a54097c9e1c2efc
blackmishra <114511660+blackmishra@users.noreply.github.com>
2023-04-25 11:57:21 +0530

    Update settings.py
    Altered static path

--- resybookingproject/settings.py
+++ resybookingproject/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # STATIC_ROOT = "/home/sas/Desktop/projects/resybookingproject/static/"
 
 # STATICFILES_DIRS = (
