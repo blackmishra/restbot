1bb1edcb6cb3ad1d39f8b0043cd692bc0338ee74
blackmishra <mishra.shashank9598@gmail.com>
2023-04-25 15:15:54 +0530

    settings altered

--- resybookingproject/settings.py
+++ resybookingproject/settings.py
@@ -158,7 +158,7 @@ STATIC_URL = '/static/'
 #     os.path.join(BASE_DIR, 'static'),
 # )
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
