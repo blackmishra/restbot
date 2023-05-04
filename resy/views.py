c5675b72f383544747b86a9fef64c8f9081d0ad6
blackmishra <mishra.shashank9598@gmail.com>
2023-05-04 22:54:13 +0530

    view file updated

--- resy/views.py
+++ resy/views.py
@@ -72,7 +72,6 @@ class SearchTemplateView(TemplateView):
         print(response.status_code)
         # data = json.loads(response.json)
         data = response.json()
-        # print(data)
 
         # All values
         values = []
@@ -88,97 +87,13 @@ class SearchTemplateView(TemplateView):
             temp['min_guests'] = item['min_party_size'],
             temp['max_guests'] = item['max_party_size'],
             temp['description'] = item['content'][0]['body']
-
             values.append(temp)
 
         # context = super(ResyTemplateView. self).get_context_data(*args, **kwargs)
         values = sorted(values, key=lambda d: d['name']) 
         self.context['data'] = values
         return self.context
-
-
-    #     # print(config_token)
-
-    #     # # Slot selection
-
-    #     # url = "https://api.resy.com/3/details"
-    #     # # config_token[i] = "rgs://resy/64872/1981324/3/2023-03-25/2023-03-15/16:30:00/2/Dining Room"
-    #     # payload = json.dumps({
-    #     #     "commit": 1,
-    #     #     "config_id": config_token[2],
-    #     #     "day": "2023-03-12",
-    #     #     "party_size": 4
-    #     # })
-    #     # headers = {
-    #     #     'authority': 'api.resy.com',
-    #     #     'accept': 'application/json, text/plain, */*',
-    #     #     'accept-language': 'en-GB,en;q=0.8',
-    #     #     'authorization': 'ResyAPI api_key="VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"',
-    #     #     'cache-control': 'no-cache',
-    #     #     'content-type': 'application/json',
-    #     #     'origin': 'https://widgets.resy.com',
-    #     #     'referer': 'https://widgets.resy.com/',
-    #     #     'sec-fetch-dest': 'empty',
-    #     #     'sec-fetch-mode': 'cors',
-    #     #     'sec-fetch-site': 'same-site',
-    #     #     'sec-gpc': '1',
-    #     #     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-    #     #     'x-origin': 'https://widgets.resy.com',
-    #     #     'x-resy-auth-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJleHAiOjE2Nzg0OTUwNzQsInVpZCI6Mzg4MDE2OTIsImd0IjoiY29uc3VtZXIiLCJncyI6W10sImV4dHJhIjp7Imd1ZXN0X2lkIjoxMjkyNjQ1MDZ9fQ.APZepm-Z5Yl8ECqA315ub0p11SJcvItCrrIZch1mNMrW3tEONvU9h2bLeodTaADeY6ojUzalNP9iQ40CKqhLWxUXAd-OUBqtsfwvSn2zukd14d9WZb1WuPZCPv_8D8jG--hMw3vVjJwvtLDwr0pAefqf_IIl7bzXc74tujLKN24DQRtC',
-    #     #     'x-resy-universal-auth': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJleHAiOjE2Nzg0OTUwNzQsInVpZCI6Mzg4MDE2OTIsImd0IjoiY29uc3VtZXIiLCJncyI6W10sImV4dHJhIjp7Imd1ZXN0X2lkIjoxMjkyNjQ1MDZ9fQ.APZepm-Z5Yl8ECqA315ub0p11SJcvItCrrIZch1mNMrW3tEONvU9h2bLeodTaADeY6ojUzalNP9iQ40CKqhLWxUXAd-OUBqtsfwvSn2zukd14d9WZb1WuPZCPv_8D8jG--hMw3vVjJwvtLDwr0pAefqf_IIl7bzXc74tujLKN24DQRtC'
-    #     # }
-
-    #     # response = requests.request("POST", url, headers=headers, data=payload)
-    #     # data = response.json()
-    #     # booking_token = data['book_token']['value']
-
-    #     # url = "https://api.resy.com/3/book"
-    #     # tk='QiUOIvEqCPoSthioWtZQKQM0W8wwg8uMqJeiHA7E8jPXW6OE0ZJBOinEkv45hnCNzFoazKuqacAnY7RS2GeOanNzOl_uk7D3x_ty96MFsXXPfucTWztDX7Mc4FuSKhNOCeDymhNQVUSRMi4Kv9v0N4Z1i1S%7CiwbEQY2nMi23lYojd0QH2xSDyvnkZdMzbG%7CxLUJ_sJJ_mlI9haYpGlMytkkCQx2k2w_g0B0_cZYbtL9eM5lSIISpq7TZ1duZgK7zgCU181NwUoK%7CFS7optqkQyoVmYRapXdDazqGKKur%7CK0S46o4%7CdrFsDJJVlvlO6j2CO%7C_1CrekK6zxNRtBtQ_7phoaRfoLNf_oCPc47vZKywa0o9kiNou0bJufVTj%7Cbkd_S9juGL_NbfqV2avQYn5iGsDjeazIE4wylwpcIXpDex3kaVFMg6Xkbg6KfPNQ5lpCiPS04qQsxPeGCHbxxrRpsTjt2unVK_IzZh6cCTZ%7CZdbOXZQUclqif79z8Z7JAj5_6XbsnXWFoXkFXtRiHA6kvh7Xp9D2P%7CuWkkiTsSfIjEpT%7CLc7dTiL6NrYa8SEP4FPDEGbHKfyYFkXV%7CFf8nX7Ji1JCAdednyPKMj73pbzU17a460_PACU68j71SoqFcs83kfRqQFJ8ePxrgAYzlQdytn8_1NGlKzT7aUB0fDtsCriA_gtOObGwXJB9Vk8PbG-aa6c78da556300ef4530985951fab3f7f2075efbbc77bc10a79e682a'
-    #     # payload=f'book_token={booking_token}&source_id=resy.com-venue-details'
-        
-    #     # # add replace = 1 in payload to update booking slot
-    #     # headers = {
-    #     #     'authority': 'api.resy.com',
-    #     #     'accept': 'application/json, text/plain, */*',
-    #     #     'accept-language': 'en-GB,en;q=0.8',
-    #     #     'authorization': 'ResyAPI api_key="VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"',
-    #     #     'cache-control': 'no-cache',
-    #     #     'content-type': 'application/x-www-form-urlencoded',
-    #     #     'origin': 'https://widgets.resy.com',
-    #     #     'referer': 'https://widgets.resy.com/',
-    #     #     'sec-fetch-dest': 'empty',
-    #     #     'sec-fetch-mode': 'cors',
-    #     #     'sec-fetch-site': 'same-site',
-    #     #     'sec-gpc': '1',
-    #     #     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
-    #     #     'x-origin': 'https://widgets.resy.com',
-    #     #     'x-resy-auth-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJleHAiOjE2ODE5OTA3MDEsInVpZCI6NDAwMzk5NjMsImd0IjoiY29uc3VtZXIiLCJncyI6W10sImV4dHJhIjp7Imd1ZXN0X2lkIjoxMzIxNDYwMTl9fQ.AFsRxEXMVczhJA5rg3dY5w_lG2I2RjywWST3hSeTV6iwataTtiOgU8DYS0BEtjLfbv2ZP00tC-gTL1J2ocu7KSU5ANIW3nzu3Axn7LWFdYVrmD4uCD_WndQFbxRZTTveXfrlifFZLAw-ckNFZOpsxdcY1Q69YEqSNbvxuZEyNYF5m94F',
-    #     #     'x-resy-universal-auth': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJleHAiOjE2ODE5OTA3MDEsInVpZCI6NDAwMzk5NjMsImd0IjoiY29uc3VtZXIiLCJncyI6W10sImV4dHJhIjp7Imd1ZXN0X2lkIjoxMzIxNDYwMTl9fQ.AFsRxEXMVczhJA5rg3dY5w_lG2I2RjywWST3hSeTV6iwataTtiOgU8DYS0BEtjLfbv2ZP00tC-gTL1J2ocu7KSU5ANIW3nzu3Axn7LWFdYVrmD4uCD_WndQFbxRZTTveXfrlifFZLAw-ckNFZOpsxdcY1Q69YEqSNbvxuZEyNYF5m94F'
-    #     # }
-    #     # print(payload)
-    #     # response = requests.request("POST", url, headers=headers, data=payload)
-
-    #     # data = response.json()
-    #     # # return Response(data, status=status.HTTP_200_OK)
-    #     # return render(request, 'index.html', {'data': values})
-
-    # # 2. Create
-    # def post(self, request, *args, **kwargs):
-    #     '''
-    #     Create the Todo with given todo data
-    #     '''
-    #     data = {
-    #         'task': request.data.get('task'), 
-    #         'completed': request.data.get('completed'), 
-    #         'user': request.user.id
-    #     }
-    #     serializer = ResySerializer(data=data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    
 
 class RestTemplateView(APIView):
     context={}
@@ -187,15 +102,9 @@ class RestTemplateView(APIView):
         
         rest_id = request.data.get('rest_id') or None
         current_date = request.data.get('booking_date') or datetime.date.today()
-        print(current_date)
-        print(rest_id)
-
         #Search specific restaurant details
         url = f"https://api.resy.com/4/find?lat=0&long=0&day={current_date}&party_size=2&venue_id={rest_id}"
-        
         payload={}
-
-
         headers = {
             'authority': 'api.resy.com',
             'accept': 'application/json, text/plain, */*',
@@ -243,20 +152,13 @@ class RestTemplateView(APIView):
         rest_details['status'] = status
         self.context['data'] = rest_details
         return Response(self.context)
-
-        # return render(request, 'find_rest.html', self.context)
-
+    
 
 class Request_booking(APIView):
 
     def get(self, request, *args, **kwargs):
-
         rest_objs = list(Restaurant.objects.all().order_by('rest_name').values())
-        # print(rest_objs)
         context = { 'data': rest_objs}
-        # context = response.context_data
-        # print(context)
-
         return render(request, 'booking.html', context) 
 
     def post(self, request, *args, **kwargs):
@@ -272,8 +174,6 @@ class Request_booking(APIView):
             'booking_available_till': datetime.date.today(),
             'resy_email': request.data.get('resy_email'),
             'resy_pwd': request.data.get('resy_pwd')
-
-
         }
 
         serializer = BookingSerializer(data=data)
@@ -283,8 +183,6 @@ class Request_booking(APIView):
             serializer.save()
             context['result'] = serializer.data
             context['message'] = 'Booking Request: Successfully created.'
-            print(context)
-
 
             return render(request, context=context, status=status.HTTP_201_CREATED, template_name='booking.html')
         context['result'] = serializer.errors
