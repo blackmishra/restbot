50ecbd848a94067508f17148bade86b96f3a3409
blackmishra <mishra.shashank9598@gmail.com>
2023-07-06 19:17:00 +0530

    const file added

---
+++ resybookingproject/constants.py
@@ -0,0 +1,14 @@
+SENDER_EMAIL = "rockdtaz@gmail.com"
+SENDER_PASSWORD = "scxywbidvtgmkwqp"
+DEFAULT_RECEPIENT = "mishra.shashank9598@gmail.com"
+AUTH_API = "https://api.resy.com/3/auth/password"
+SEARCH_API = "https://api.resy.com/3/venuesearch/search"
+FIND_REST_API = "https://api.resy.com/4/find?lat=0&long=0&day="
+DETAILS_API = "https://api.resy.com/3/details"
+BOOKING_API = "https://api.resy.com/3/book"
+CANCEL_API = "https://api.resy.com/3/cancel"
+
+IMG_NOT_AVAIALABLE_ICON = (
+    "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png"
+)
+
