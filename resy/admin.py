02d93afa7c1ead3400eb10cc57fc5f52e6ebd38e
blackmishra <mishra.shashank9598@gmail.com>
2023-07-09 01:30:55 +0530

    wu_model added

--- resy/admin.py
+++ resy/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Resy, Reservation_request, Restaurant, User
+from .models import Booking_details, Resy, Reservation_request, Restaurant, User
 
 
 # Register your models here.
 admin.site.register(Reservation_request)
 admin.site.register(Restaurant)
 admin.site.register(User)
+admin.site.register(Booking_details)
+
